5d267c4bb8503fccb260ac9151e7e56994599039
iRUNCHIKK <btkv_irina@icloud.com>
2024-04-15 01:32:58 +0500

    Completed tasks

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.lang.String;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //task1
+        System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        //task2
+        System.out.println("Task 2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        //task3
+        System.out.println("Task 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
